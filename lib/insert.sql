e9532032a21392ff7da8295439c4f699ac488390
Jason Whitlock <jrwhitlock7@gmail.com>
2021-04-01 00:11:54 -0700

    commit tree 0ea2f21e975ece8c0658bd2eb2a1671e1a706920

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Angry", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "F", "Black", "Happy", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, 'F', 'Brown', 'Happy', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, 'M', 'Blue', 'Content', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, 'F', 'White', 'Chill', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, 'M', 'Green', 'Salty', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, 'F', 'Yellow', 'Happy', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (Null, 1, 'M', 'Black', 'Happy', true);
